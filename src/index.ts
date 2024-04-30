b61ad9a0b2870a329162cb9f9de05b74ece9857d
yyh <elaine20040930@gmail.com>
2024-04-30 19:19:44 +0800

    feat(ts): 利用物件記錄產品資料

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-let productId: number;
-productId = 1;
-console.log("Product Id = ", productId);
-
-const productName: string = "圖像 Angular 開發入門 第二版";
-console.log("Product Name = ", productName);
-
-const isShow = true;
-console.log("Is Show = ", isShow);
-
-let createDate = new Date();
-console.log("Create Date = ", createDate);
-
-let modifyDate: Date | undefined;
-console.log("Modify Date = ", modifyDate);
+const product = {
+  id: 1,
+  name: "圖像 Angular 開發入門 第二版",
+  company: "博碩文化",
+  price: 500,
+  isShow: true,
+  createDate: 2024 - 4 - 10,
+  modifyDate: undefined,
+};
+console.log(product);
+console.table(product);
