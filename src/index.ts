9ff881cc098fb88c064f2a02abf7b628f531eb9f
yyh <elaine20040930@gmail.com>
2024-04-30 19:10:36 +0800

    feat(ts): 利用聯集變數記錄產品修改日期

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,6 @@ console.log("Is Show = ", isShow);
 
 let createDate = new Date();
 console.log("Create Date = ", createDate);
+
+let modifyDate: Date | undefined;
+console.log("Modify Date = ", modifyDate);
