18a477c72dc5802464bc5a849e9240d0411064eb
yyh <elaine20040930@gmail.com>
2024-04-30 19:38:37 +0800

    feat(ts): 利用類別定義產品資料

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-const product = {
-  id: 1,
-  name: "圖像 Angular 開發入門 第二版",
-  company: "博碩文化",
-  price: 500,
-  isShow: true,
-  createDate: 2024 - 4 - 10,
-  modifyDate: undefined,
-};
+import { Product } from "./product";
+
+const product = new Product(
+  1,
+  "圖像 Angular 開發入門 第二版",
+  "博碩文化",
+  500,
+  true,
+  new Date(2024, 3, 10),
+  undefined
+);
 console.log(product);
 console.table(product);
